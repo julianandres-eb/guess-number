3f520c0282efc3bdc142752b41c37e9775c60abe
Julian Andres Lucatelli <julianandreslucatelli@gmail.com>
2019-05-07 18:28:53 +0200

    Creation of Thinker

---
+++ bin/thinker/thinker/console.py
@@ -0,0 +1,18 @@
+import
+def main():
+
+    # Instructions and Rules
+    print("------------------------------------------------------------------------------------")
+    print("Welcome to 'Thinker', our 'tiny' game that guesses numbers from the user")
+    print("Let's get started!")
+    print("The instructions are the followings:")
+    print("- We'll start making some questions like 'How many digits has the number?'")
+    print("- Then we make our stuff trying to determine the 'secret number'")
+    print("- After that you'll tell us, if the number is correct. If not, we'll keep bothering you with questions until we determine the number")
+    print("------------------------------------------------------------------------------------")
+
+    print("As we said before, we need to know something to start our process, let's help us!")
+
+
+if __name__ == '__main__':
+    main()
--- bin/thinker/thinker/thinker.py
+++ bin/thinker/thinker/thinker.py
@@ -1,17 +1,3 @@
 
-
-def main():
-
-    # Instructions and Rules
-    print("Welcome to 'Thinker', our 'tiny' game that guesses numbers from the user")
-    print("Let's get started!")
-    print("The instructions are the followings:")
-    print("- We'll start making some questions like 'How many digits has the number?'")
-    print("- Then we make our stuff trying to determine the 'secret number'")
-    print("- After that you'll tell us, if the number is correct. If not, we'll keep bothering you with questions until we determine the number")
-
-    # Console Options
-
-
-if __name__ == '__main__':
-    main()
+def thinker():
+    print("aaaa")
