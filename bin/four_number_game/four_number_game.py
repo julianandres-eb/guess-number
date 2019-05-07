d6e5e965ecf85e676dbb2c485e088882e7d038bf
Julian Andres Lucatelli <julianandreslucatelli@gmail.com>
2019-05-07 18:06:20 +0200

    First Implementation of Thinker

--- bin/four_number_game/four_number_game.py
+++ bin/four_number_game/four_number_game.py
@@ -63,26 +63,24 @@ def calculateCorrectAndRegular(shot, correctNumber):
     return [corrects, regulars]
 
 # Instructions
-# -------------------------------------------------------------------------
-# Welcome to "4NG", our "tiny" game to guess which number we're hiding to you
-# Let's get started!
 
-# The instructions are the following:
+print("------------------------------------------------------------------------------------")
 
-# - You have to guess a number between 0 and 9999 (that means 4 digits)
+print("Welcome to '4NG', our 'tiny' game to guess which number we're hiding to you")
+print("Let's get started!")
+print("The instructions are the following:")
+print("- You have to guess a number between 0 and 9999 (that means 4 digits)")
+print("- These 4 digits cannot be repeated, for example: 1224")
+print("- After you tell us, which number you think is correct, we'll help you by saying")
+print("how many numbers are in the right position (Good) and how many correspond to")
+print("the correct number but they aren't placed properly (Regular).")
+print("For example: if the correct number is 1234 and you write down 1273,")
+print("our response will be '2 Good, 1 Regular'")
 
-# - These 4 digits cannot be repeated, for example: 1224
-
-# - After you tell us, which number you think is correct, we'll help you by saying
-#   how many numbers are in the right position (Good) and how many correspond to
-#   the correct number but they aren't placed properly (Regular).
-#
-#   For example: if the correct number is 1234 and you write down 1273,
-#   our response will be "2 Good, 1 Regular"
-# -------------------------------------------------------------------------
+print("------------------------------------------------------------------------------------")
 
 # Variables
-guessing = False # Variable used to continue (or finish) the guessing process
+guessing = False
 numberToGuess = generateNumber()
 
 while guessing is False:
