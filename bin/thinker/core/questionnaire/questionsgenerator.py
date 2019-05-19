f45a9d30f511006c8adc5a6097f8f414ed9ea3d0
Julian Andres Lucatelli <julianandreslucatelli@gmail.com>
2019-05-19 19:44:53 +0200

    Comments in QuestionsGenerator

--- bin/thinker/core/questionnaire/questionsgenerator.py
+++ bin/thinker/core/questionnaire/questionsgenerator.py
@@ -8,39 +8,58 @@ from bin.thinker.core.questionscreator.creatorbetweenquestion import CreatorBetw
 from bin.thinker.core.questionscreator.creatorpositionquestion import CreatorPositionQuestion
 from bin.thinker.core.questionscreator.creatordigitsquestion import CreatorDigitsQuestion
 
+
+###########################################################
+#
+# QuestionsGenerator
+#
+# Singleton class that generates all possible questions in program
+#
+###########################################################
+
 class QuestionsGenerator(metaclass=Singleton):
+
+    # Declaration of class variables
     questions = []
 
+    # Init class
     def __init__(self):
         self.questions = self.generateQuestions()
 
-    def setQuestions(self, questions):
-        self.questions = questions
-
-    def getQuestions(self):
-        return self.questions
-
-    def getQuestion(self, key):
-        questionToReturn = []
-        for q in self.questions:
-            if q.key == key:
-                questionToReturn.append(q)
-                break
-        return questionToReturn
-
-    def composeNameClass(self, key):
-        return "Creator" + key.capitalize() + "Question"
-
     def generateQuestions(self):
-        questionsToReturn = []
+
+        # Declaration of class variables
+        questionsToReturn:list = []
 
         # Create Questions from File
         # - Read file and generate a dict
         with open(Utils.getQuestionsPath(), 'r') as JSON:
             allQuestions = json.load(JSON)
 
+        # For each question, we create the questions model
         for key, value in allQuestions.items():
-            creator = globals()[self.composeNameClass(key)]()
-            questionsToReturn.append(creator._createQuestion(value))
+            try:
+                # We create the class who manages the creation
+                creator = globals()[self.composeNameClass(key)]()
+
+            # If there's not that creator, we keep going
+            except ModuleNotFoundError:
+                pass
+
+            # Otherwise we append it
+            else:
+                questionsToReturn.append(creator._createQuestion(value))
 
         return questionsToReturn
+
+    def setQuestions(self, questions):
+        self.questions = questions
+
+    def getAllQuestions(self):
+        return self.questions
+
+    def getQuestionsWithKey(self, key):
+        return [q for q in self.questions if q.key is key]
+
+    def composeNameClass(self, key):
+        return "Creator" + key.capitalize() + "Question"
