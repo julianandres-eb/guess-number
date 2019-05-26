23baa8d06745516e5af1afd4890e57dafbe1d463
Julian Andres Lucatelli <julianandreslucatelli@gmail.com>
2019-05-26 21:00:52 +0200

    Changes in Path to correctly execute tests

--- thinker/utils/utils.py
+++ thinker/utils/utils.py
@@ -3,16 +3,19 @@ import os
 
 class Utils:
 
+    baseDirectory: str
+
     def __init__(self):
-        pass
+        self.baseDirectory = os.getcwd()
 
-    def getQuestionsPath():
-        return os.getcwd() + "/thinker/model/question/listQuestions.json"
+    def getQuestionsPath(self):
+        return self.baseDirectory + "/thinker/model/question/listQuestions.json"
 
-    def primes(self):
+
+    def primes(self, bound=10):
         primes = [2]
 
-        for i in range(3, 10, 2):
+        for i in range(3, bound, 2):
             if not any(i % prime == 0 for prime in primes):
                 primes.append(i)
 
