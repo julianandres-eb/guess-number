f0d23f5c75b0fbbbd4a72951fa5b41c95f8c6b33
Julian Andres Lucatelli <julianandreslucatelli@gmail.com>
2019-05-26 22:16:57 +0200

    First implementation of Test Unit - NumberBetweenGenerator

--- thinker/test/test_core/test_numbergenerator/test_numberbetweengenerator.py
+++ thinker/test/test_core/test_numbergenerator/test_numberbetweengenerator.py
@@ -1,20 +1,16 @@
 import unittest
-
-from thinker.core.numbergenerator.numberbetweengenerator import GeneratorBetweenNumbers as gnb
-
+from thinker.core.numbergenerator.numberbetweengenerator import GeneratorBetweenNumbers
 
 class TestNumberBetweenGenerator(unittest.TestCase):
 
-    allValues: list
+    def test_generateNumberWithOneYesBoundary(self):
+        allValues: list = [i for i in range(1, 10)]
+        self.assertListEqual(GeneratorBetweenNumbers().generateNumbers([[2, 6, "y"]], allValues), [2, 3, 4, 5, 6])
 
-    def __init__(self):
-        super().__init__()
-        self.allValues = [i for i in range(1, 20)]
+    def test_generateNumberWithOneNoBoundary(self):
+        allValues: list = [i for i in range(1, 10)]
+        self.assertListEqual(GeneratorBetweenNumbers().generateNumbers([[2, 6, "n"]], allValues), [1, 7, 8, 9])
 
-    def test_generateNumberWithYesBoundary(self):
-        self.assertIs(gnb().generateNumbers([2, 6, 'y'], self.allValues), [2, 3, 4, 5, 6])
-
-    ## casos positivos
-    ## - solo con y
-    ## - solo con n
-    ## - uno de cada uno
+    def test_generateNumberWithOneYesBoundaryAndOneNoBoundary(self):
+        allValues: list = [i for i in range(1, 10)]
+        self.assertListEqual(GeneratorBetweenNumbers().generateNumbers([[2, 6, "y"], [4, 8, "n"]], allValues), [2, 3])
