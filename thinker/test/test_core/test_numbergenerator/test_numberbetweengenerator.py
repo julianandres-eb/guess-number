3fcdfebf6d195d9eb0e0f3dc07825ee5d15b6498
Julian Andres Lucatelli <julianandreslucatelli@gmail.com>
2019-05-27 19:23:15 +0200

    Use 'patch.object' to mock QuestionSolver.digits in Test Unit of NumberBetweenGenerator

--- thinker/test/test_core/test_numbergenerator/test_numberbetweengenerator.py
+++ thinker/test/test_core/test_numbergenerator/test_numberbetweengenerator.py
@@ -1,21 +1,22 @@
 import unittest
+
 from unittest.mock import patch
 from thinker.core.numbergenerator.numberbetweengenerator import GeneratorBetweenNumbers
 from thinker.core.questionsolver.questionsolver import QuestionSolver
 
 class TestNumberBetweenGenerator(unittest.TestCase):
 
+    @patch.object(QuestionSolver, 'digits', 1)
     def test_generateNumberWithOneYesBoundary(self):
         allValues: list = [i for i in range(1, 10)]
-        with patch.object(QuestionSolver, 'digits', 1):
-            assert GeneratorBetweenNumbers().generateNumbers([[2, 6, "y"]], allValues) == [2, 3, 4, 5, 6]
+        assert GeneratorBetweenNumbers().generateNumbers([[2, 6, "y"]], allValues) == [2, 3, 4, 5, 6]
 
+    @patch.object(QuestionSolver, 'digits', 1)
     def test_generateNumberWithOneNoBoundary(self):
         allValues: list = [i for i in range(1, 10)]
-        with patch.object(QuestionSolver, 'digits', 1):
-            assert GeneratorBetweenNumbers().generateNumbers([[2, 6, "n"]], allValues) == [1, 7, 8, 9]
+        assert GeneratorBetweenNumbers().generateNumbers([[2, 6, "n"]], allValues) == [1, 7, 8, 9]
 
+    @patch.object(QuestionSolver, 'digits', 1)
     def test_generateNumberWithOneYesBoundaryAndOneNoBoundary(self):
         allValues: list = [i for i in range(1, 10)]
-        with patch.object(QuestionSolver, 'digits', 1):
-            assert GeneratorBetweenNumbers().generateNumbers([[2, 6, "y"], [4, 8, "n"]], allValues) == [2, 3]
+        assert GeneratorBetweenNumbers().generateNumbers([[2, 6, "y"], [4, 8, "n"]], allValues) == [2, 3]
